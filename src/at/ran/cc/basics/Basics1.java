e979e20d6cdbc782953ce8262e92465b39a81819
jrwaheed <j.r.waheed@gmail.com>
2021-03-10 20:20:20 +0100

    Bankomat - finished

--- src/at/ran/cc/basics/Basics1.java
+++ src/at/ran/cc/basics/Basics1.java
@@ -1,5 +1,6 @@
 package at.ran.cc.basics;
 
+import org.w3c.dom.ls.LSOutput;
 
 import java.awt.font.NumericShaper;
 import java.sql.SQLOutput;
@@ -7,53 +8,53 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Basics1 {
+    static int currentBalance = 0;
 
-
-
-
-    static int Deposit(int depositAmount) {
-        int currentBalance = 1000;
-        int newBalance = depositAmount + currentBalance;
-        System.out.println("Your new balance:" + newBalance);
-        return newBalance;
+    static void addToBalance() {
+        System.out.println("Enter your deposit amount");
+        Scanner DepositObj = new Scanner(System.in);
+        int depositAmount = DepositObj.nextInt();
+        currentBalance = depositAmount + currentBalance;
+        System.out.println("Your new balance:" + currentBalance);
     }
 
-    static int Withdrawal(int withdrawalAmount) {
-        int currentBalance = 1000;
-        int newBalance = -withdrawalAmount + currentBalance;
-        System.out.println("Your new balance:" + newBalance);
-        return newBalance;
+    static void withdrawFromBalance() {
+        System.out.println("Enter your withdrawal amount");
+        Scanner withdrawalObj = new Scanner(System.in);
+        int withdrawalAmount = withdrawalObj.nextInt();
+        currentBalance = -withdrawalAmount + currentBalance;
+        System.out.println("Your new balance:" + currentBalance);
+    }
+
+    static void printBalance() {
+        System.out.println("Your current balance is:" + currentBalance);
+    }
+
+    static void printError() {
+        System.out.println("Incorrect entry. Try again");
+    }
+
+    static void Begin() {
+        boolean isFinished = false;
+        while (!isFinished) {
+            System.out.print("Please make your selection: \n 1) Deposit\n 2) Withdrawal\n 3) Account Balance\n 4) Exit \n");
+            Scanner InitialObj = new Scanner(System.in);
+            String userInitialEntry = InitialObj.nextLine();
+            switch (userInitialEntry) {
+                case "1" -> addToBalance();
+                case "2" -> withdrawFromBalance();
+                case "3" -> printBalance();
+                case "4" -> {
+                    System.out.println("Thank you for your business. Goodbye.");
+                    isFinished = true;
+                }
+                default -> printError();
+            }
+        }
     }
 
     public static void main(String[] args) {
-
-
-        System.out.print("Please make your selection: \n 1) Deposit\n 2) Withdrawal\n 3) Account Balance\n 4) Exit \n");
-        Scanner InitialObj = new Scanner(System.in);
-        String userInitialEntry = InitialObj.nextLine();
-        System.out.println("Here is the user entry: " + userInitialEntry);
-
-        switch (userInitialEntry) {
-            case "1":
-                System.out.println("Enter your deposit amount");
-                Scanner DepositObj = new Scanner(System.in);
-                int depositAmount = DepositObj.nextInt();
-                Deposit(depositAmount);
-                break;
-
-            case "2":
-                System.out.println("Enter your withdrawal amount");
-                Scanner withdrawalObj = new Scanner(System.in);
-                int withdrawalAmount = withdrawalObj.nextInt();
-                Withdrawal(withdrawalAmount);
-                break;
-            case "3":
-                //System.out.println("Your current balance is:" + newBalance); break;
-            case "4":
-                System.out.println("Thank you for your business. Goodbye."); break;
-            default: break;}
-
+        Begin();
     }
-
-
 }
+
