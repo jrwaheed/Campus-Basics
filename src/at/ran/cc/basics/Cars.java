e331f5bdb2d0bd07c3f03fe046e578a01fab235e
jrwaheed <j.r.waheed@gmail.com>
2021-04-22 19:49:41 +0200

    Battleship - small correction made

---
+++ src/at/ran/cc/basics/Cars.java
@@ -0,0 +1,17 @@
+package at.ran.cc.basics;
+
+public class Cars {
+
+    public int fuelCapacity;
+    public int fuelConsumption;
+    public boolean isLuxury;
+    public String vehicleBrand;
+    public String color;
+    public int price;
+
+    public void joyRiding() {
+        this.fuelCapacity = this.fuelCapacity - this.fuelConsumption;
+    }
+
+
+}
---
+++ src/at/ran/cc/basics/Scratch.java
@@ -0,0 +1,31 @@
+package at.ran.cc.basics;
+
+public class Scratch {
+
+    public static void main(String [] args) {
+        Cars car1 = new Cars();
+        car1.vehicleBrand = "Jaguar";
+        car1.color = "White";
+        car1.fuelCapacity = 50;
+        car1.fuelConsumption = 6;
+        car1.isLuxury = true;
+
+        Cars car2 = new Cars();
+        car2.vehicleBrand = "Honda";
+        car2.color = "Red";
+        car2.fuelCapacity = 70;
+        car2.fuelConsumption = 3;
+        car2.isLuxury = false;
+
+        System.out.println("Started out with " + car1.fuelCapacity + " liters when we started.");
+        car1.joyRiding();
+        System.out.println("Ended with " + car1.fuelCapacity + ".");
+        if(car1.isLuxury) {
+            System.out.println("But at least its a luxury ride!");
+        } else {
+            System.out.println("But damn. Its a beater.");
+        }
+
+    }
+
+}
