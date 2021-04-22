d40b6c1babd37ee3eb124fe35abfc59fe9299dc4
jrwaheed <j.r.waheed@gmail.com>
2021-04-22 21:55:32 +0200

    Work on "This"

--- src/at/ran/cc/basics/Cars.java
+++ src/at/ran/cc/basics/Cars.java
@@ -2,17 +2,45 @@ package at.ran.cc.basics;
 
 public class Cars {
 
-    public int fuelCapacity;
-    public int fuelConsumption;
+    // Objektvariablern GEDÄCHTNIS
+    public double fuelCapacity;
+    public double fuelConsumption;
     public boolean isLuxury;
     public String vehicleBrand;
-    public String color;
+    public String color = "white";
     public int price;
+    public  double currentFuelLevel;
 
 
     public void joyRiding() {
         this.fuelCapacity = this.fuelCapacity - this.fuelConsumption;
     }
 
+    public void pumpTheBrakes() {
+        System.out.println("I brake");
+    }
+
+    public void turboBoost() {
+        if((this.currentFuelLevel / this.fuelCapacity) > .1 ) {
+            System.out.println("Punch it, brah!");
+        } else {
+            System.out.println("Running of E, Partner.");
+        }
+    }
+
+    public void honk(int repetitions) {
+        for (int i = 0; i < repetitions; i++) {
+            System.out.println("HONK!");
+        }
+    }
+
+    public void remainingRange() {
+        double remainingRange = ((this.currentFuelLevel/this.fuelConsumption) * 100);
+        System.out.println("Your current range is " + remainingRange);
+    }
+
+    public Cars(String color) {
+       this.color = color;
+    }
 
 }
--- src/at/ran/cc/basics/Scratch.java
+++ src/at/ran/cc/basics/Scratch.java
@@ -4,20 +4,28 @@ public class Scratch {
 
     public static void main(String [] args) {
 
-        Cars car1 = new Cars();
+        Cars car1 = new Cars("green");
         car1.vehicleBrand = "Jaguar";
         car1.color = "White";
         car1.fuelCapacity = 50;
         car1.fuelConsumption = 6;
         car1.isLuxury = true;
+        car1.currentFuelLevel = 2;
 
-        Cars car2 = new Cars();
+        Cars car2 = new Cars("red");
         car2.vehicleBrand = "Honda";
         car2.color = "Red";
         car2.fuelCapacity = 70;
         car2.fuelConsumption = 3;
         car2.isLuxury = false;
+        car2.currentFuelLevel = 50;
 
+
+
+        Cars car3 = new Cars("blue");
+
+        System.out.println(car1.color);
+        /*
         System.out.println("Started out with " + car1.fuelCapacity + " liters when we started.");
         car1.joyRiding();
         System.out.println("Ended with " + car1.fuelCapacity + ".");
@@ -26,7 +34,16 @@ public class Scratch {
         } else {
             System.out.println("But damn. Its a beater.");
         }
+        car2.pumpTheBrakes();
+        car1.turboBoost();
+        car2.turboBoost();
+        car1.honk(4);
+        car1.remainingRange();
+        car2.remainingRange();
 
+         */
     }
+
+
 
 }
