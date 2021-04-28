0daa48750b551353c1b4fc26a551c26d8c3a4ec1
jrwaheed <j.r.waheed@gmail.com>
2021-04-28 21:42:06 +0200

    Big OOP move

---
+++ src/at/ran/cc/OO/Tires.java
@@ -0,0 +1,26 @@
+package OO;
+
+public class Tires {
+    public enum QUALITY {low, medium, high}
+    private QUALITY rubberQuality;
+    private double innerDiameter;
+    private double outDiameter = 100;
+
+    public Tires(QUALITY rubberQuality, double innerDiameter, double outDiameter) {
+        this.rubberQuality = rubberQuality;
+        this.innerDiameter = innerDiameter;
+        this.outDiameter = outDiameter;
+    }
+
+    public QUALITY getRubberQuality() {
+        return rubberQuality;
+    }
+
+    public double getInnerDiameter() {
+        return innerDiameter;
+    }
+
+    public double getOutDiameter() {
+        return outDiameter;
+    }
+}
